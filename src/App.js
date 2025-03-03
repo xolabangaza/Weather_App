b258cfea23472dc34d034f225cf579e0be948483
Xola Bangaza <Xola.Bangaza@redacademy.co.za>
2025-03-03 09:21:11 +0200

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WeatherMap from "./WeatherMap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center", padding: "10px", background: "#282c34", color: "white" }}>
+                Weather Map App
+            </h1>
+            <WeatherMap />
+        </div>
+    );
+};
 
 export default App;
